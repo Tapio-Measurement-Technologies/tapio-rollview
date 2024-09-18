29b533a4cbd8e10bc247c38fb75ecfcf1b69457b
Leo Immonen <leo.immonen@tapiotechnologes.com>
2024-09-18 16:04:33 +0300

    Handle dynamic import exceptions

--- src/utils/dynamic_loader.py
+++ src/utils/dynamic_loader.py
@@ -3,16 +3,20 @@ import os
 
 def load_modules_from_folder(folder_path):
     modules = {}
-    for filename in os.listdir(folder_path):
-        if filename.endswith('.py') and filename != '__init__.py':
-            module_name = filename[:-3]
-            module_path = os.path.join(folder_path, filename)
-            spec = importlib.util.spec_from_file_location(module_name, module_path)
-            module = importlib.util.module_from_spec(spec)
-            spec.loader.exec_module(module)
-            if hasattr(module, 'description') and callable(getattr(module, 'description', None)):
-                module.description = module.description()
-            if hasattr(module, 'enabled'):
-                module.enabled = module.enabled
-            modules[module_name] = module
+    try:
+        for filename in os.listdir(folder_path):
+            if filename.endswith('.py') and filename != '__init__.py':
+                module_name = filename[:-3]
+                module_path = os.path.join(folder_path, filename)
+                spec = importlib.util.spec_from_file_location(module_name, module_path)
+                module = importlib.util.module_from_spec(spec)
+                spec.loader.exec_module(module)
+                if hasattr(module, 'description') and callable(getattr(module, 'description', None)):
+                    module.description = module.description()
+                if hasattr(module, 'enabled'):
+                    module.enabled = module.enabled
+                modules[module_name] = module
+                print(f"Loaded postprocessor '{module_name}'")
+    except Exception as e:
+        print(f"Failed to load postprocessors: {e}")
     return modules
